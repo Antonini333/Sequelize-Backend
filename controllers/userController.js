860ea9bc8f35e15cd71a73a91b599b63380cbb13
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-10-19 22:35:42 +0200

    Added login endpoint and JWT tokens

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,31 @@ async signup(req, res){
             error, message: "Something went wrong creating user"
         })
     }
+},
+
+async login (req, res) {
+    try {
+        const user = await User.findOne({
+            where:{
+                email: req.body.email
+            }
+        })
+        if(!user) {
+            return res.status(400).send({
+                message: "Wrong email"
+            })
+        }
+        const token = jwt.sign({id: user.id}, "agallaselperrocobarde", {expiresIn: "30d"})
+        console.log(token)
+        user.token = token;
+        await user.save()
+        res.send(user);
+    }catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: "Wrong credentials"
+        })
+    }
 }
 
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const UserController = require ('../controllers/userController');
 
 router.post('/signup', UserController.signup);
+router.post('/login', UserController.login);
 
 
 
